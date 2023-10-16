7ed31ac4add73463d15aa58e0d519566292cf206
Kerem Deen <kerem.deen@gmail.com>
2023-10-16 11:09:35 -0400

    add repr and suits

--- lib/player.py
+++ lib/player.py
@@ -6,14 +6,5 @@ class Player():
         self.name = name
         self.hand = []
     
-    def get_hand_value(self):
-        total = 0
-        for card in self.hand:
-            if card.value > 10:
-                total += 10
-            else:
-                total += card.value
-        return total
-    
     def new_card(self):
         Card(self)
